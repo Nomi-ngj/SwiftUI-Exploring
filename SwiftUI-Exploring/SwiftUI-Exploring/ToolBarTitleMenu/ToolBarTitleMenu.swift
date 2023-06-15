3bfba08e878471c6324765c1b059fca193321de1
Nouman Gul <Noumanguljunejo@gmail.com>
2023-06-15 14:10:19 +0500

    Adding dynamic name on toolbar or selectable toolbar

---
+++ SwiftUI-Exploring/SwiftUI-Exploring/ToolBarTitleMenu/ToolBarTitleMenu.swift
@@ -0,0 +1,32 @@
+//
+//  ToolBarTitleMenu.swift
+//  SwiftUI-Exploring
+//
+//  Created by Nouman Gul Junejo on 15/06/2023.
+//
+
+import SwiftUI
+
+struct ToolBarTitleMenu: View {
+    @State private var title = "Selected Title 0"
+    var body: some View {
+        NavigationStack{
+            Text("Nouman Gul Junejo")
+                .navigationTitle($title)
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbarTitleMenu {
+                    ForEach(0..<8) { index in
+                        Button {
+                            title = "Selected Title \(index)"
+                        } label: {
+                            Text("Title \(index)")
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ToolBarTitleMenu()
+}
